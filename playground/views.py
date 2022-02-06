76e1f6a167d541877270fb3153b782a26557a0e2
Bung Danar <danar.g.putera@gmail.com>
2022-02-06 08:22:55 +0700

    chore: First init at the beginning of Django ORM section

---
+++ playground/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def say_hello(request):
+    return render(request, 'hello.html', {
+        'name': 'Bung Danar'
+    })
