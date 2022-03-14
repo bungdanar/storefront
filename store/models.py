3527200f28ca8f16827a991964f646807878ca6c
Bung Danar <danar.g.putera@gmail.com>
2022-03-14 21:12:02 +0700

    End of Section 2

--- store/models.py
+++ store/models.py
@@ -31,7 +31,8 @@ class Product(models.Model):
         validators=[MinValueValidator(1)]
     )
     last_update = models.DateTimeField(auto_now=True)
-    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    collection = models.ForeignKey(
+        Collection, on_delete=models.PROTECT, related_name='products')
     promotions = models.ManyToManyField(Promotion, blank=True)
 
     def __str__(self) -> str:
